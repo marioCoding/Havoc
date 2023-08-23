f2cdf90e09536dc054768e5e84d4b677844466e7
marioCoding <marosw.sal@gmail.com>
2023-08-23 11:51:10 -0700

    updated api fetch url's

--- js/main.js
+++ js/main.js
@@ -27,7 +27,7 @@ async function getData(event) {
 /*  This code is making an API request to the Blizzard API to fetch data about a World of
   Warcraft character's character appearance */
       
-      let url0 = `https://us.api.blizzard.com/profile/wow/character/${requestRealmName}/${requestCharName}/appearance?namespace=profile-us&locale=en_US&access_token=USkJV9J487uokW93PqhwB3Ka9CQCikPvtD`;
+      let url0 = `https://us.api.blizzard.com/profile/wow/character/${requestRealmName}/${requestCharName}/appearance?namespace=profile-us&locale=en_US&access_token=USXKXQSiqHxN6R39kxn3plUh5QWh3VFkio`;
       let response0 = await fetch(url0);
       let appearanceData = await response0.json();
       let charClass;
@@ -40,11 +40,9 @@ async function getData(event) {
 /* This code is making an API request to the Blizzard API to fetch data about a World of
   Warcraft character's specialization. */
       
-      let url1 = `https://us.api.blizzard.com/profile/wow/character/${requestRealmName}/${requestCharName}/specializations?namespace=profile-us&locale=en_US&access_token=USkJV9J487uokW93PqhwB3Ka9CQCikPvtD`;
+      let url1 = `https://us.api.blizzard.com/profile/wow/character/${requestRealmName}/${requestCharName}/specializations?namespace=profile-us&locale=en_US&access_token=USXKXQSiqHxN6R39kxn3plUh5QWh3VFkio`;
       let response1 = await fetch(url1);
-      let specData = await response1.json();
-      
-      if (event.request.method == 'GET') {
+      let specData = await response1.json()
 
         let spec1;
         let spec2;
@@ -56,37 +54,54 @@ async function getData(event) {
         } catch {
           err => console.error("oops!", err.message);
         }
+  
+/* This code is making an API request to the Blizzard API to fetch data about a World of Warcraft
+  character's avatar. */
+      
+    let url2 = `https://us.api.blizzard.com/profile/wow/character/${requestRealmName}/${requestCharName}/character-media?namespace=profile-us&locale=en_US&access_token=USXKXQSiqHxN6R39kxn3plUh5QWh3VFkio`;
+    let response2 = await fetch(url2);
+    let avatarData = await response2.json();
+    try {
+      // Container Section 1 - Character Name
+      const avatarHeader = document.createElement("h3");
+      const avatarSubHeader = document.createElement("h4");
+      const avatarElement = document.createElement("img");
+      let avatarName = await avatarData.character.name;
+      let avatarRealm = await avatarData.character.realm.name;
+      let avatarURL = await avatarData.assets[2].value;
+      avatarElement.src = avatarURL;
+
+      // Style container 1
+      avatarHeader.innerText = avatarName;
+      avatarHeader.style.fontFamily = "warcraft, sans-serif";
+      avatarHeader.style.fontSize = "300%";
+      avatarHeader.style.background = "#9CA6B0";
+      avatarSubHeader.innerText = avatarRealm;
+      avatarSubHeader.style.fontFamily = "warcraft, sans-serif";
+      avatarSubHeader.style.fontSize = "200%";
+      avatarSubHeader.style.background = "#9CA6B0";
+      avatarElement.style.width = "100%";
+      avatarElement.style.background = "black";
+      avatarElement.style.margin = "5%";
+      avatarElement.style.borderRadius = "50px";
+      avatarElement.style.borderStyle = "inset";
+
+      // make reference to the html containers where the info will be displayed
+      container2.innerHTML = " ";
+      container2.appendChild(avatarHeader);
+      container2.appendChild(avatarSubHeader);
+      container2.appendChild(avatarElement);
+      } catch {
+        err => console.error("oops!", err.message);
       }
 
-/* This code making an API request to the Blizzard API to fetch data about a World of
+/* This code is making an API request to the Blizzard API to fetch data about a World of
   Warcraft character's PvP statistics. */
   
-      let url2 = `https://us.api.blizzard.com/profile/wow/character/${requestRealmName}/${requestCharName}/pvp-bracket/3v3?namespace=profile-us&locale=en_US&access_token=USkJV9J487uokW93PqhwB3Ka9CQCikPvtD`;
-      let response2 = await fetch(url2);
-      let pvpData = await response2.json();
+      let url3 = `https://us.api.blizzard.com/profile/wow/character/${requestRealmName}/${requestCharName}/pvp-bracket/3v3?namespace=profile-us&locale=en_US&access_token=USXKXQSiqHxN6R39kxn3plUh5QWh3VFkio`;
+      let response3 = await fetch(url3);
+      let pvpData = await response3.json();
       try {
-        // Container Section 1 - Character Name
-        const nameHeader = document.createElement("h3");
-        const nameElement = document.createElement("p");
-        let nameData = await pvpData.character.name;
-        nameHeader.innerText = nameData;
-        // Style container 1
-          nameHeader.style.fontFamily = "warcraft, sans-serif";
-          nameHeader.style.fontSize = "200%";
-          nameHeader.style.background = "#B0A59C";
-          nameElement.style.fontSize =   "150%";
-
-        // Container Section 2 - Realm Name
-        const realmHeader = document.createElement("h4");
-        const realmElement = document.createElement("p");
-        let realmData = await pvpData.character.realm.slug;
-        realmHeader.innerText = realmData;
-        // Style container 2
-          realmHeader.style.fontFamily = "warcraft, sans-serif";
-          realmHeader.style.fontSize = "200%";
-          realmHeader.style.background = "#B0A59C";
-          realmElement.style.fontSize = "150%";
-
         // Container Section 3 - Rating
         const ratingHeader = document.createElement("h4");
         const ratingElement = document.createElement("p");
@@ -94,8 +109,8 @@ async function getData(event) {
         ratingElement.innerText = ratingData;
         // Style container 3
         ratingHeader.innerHTML = "3v3 Rating: ";
-        ratingHeader.style.background = "#B0A59C";
-        ratingElement.style.background = "#B0A59C";
+        ratingHeader.style.background = "#9CA6B0";
+        ratingElement.style.background = "#9CA6B0";
         ratingElement.style.fontFamily = "warcraft, sans-serif";
         ratingElement.style.fontSize = "150%";
 
@@ -111,33 +126,29 @@ async function getData(event) {
         let gamesWon = await pvpData.season_match_statistics.won;
         let gamesLost = await pvpData.season_match_statistics.lost;
         statsElement1Title.innerHTML = "Games played: ";
-        statsElement1Title.style.background = "#B0A59C";
+        statsElement1Title.style.background = "#9CA6B0";
         statsElement1.innerText = gamesPlayed;
-        statsElement1.style.background = "#B0A59C";
+        statsElement1.style.background = "#9CA6B0";
         statsElement2Title.innerHTML = "Won: ";
-        statsElement2Title.style.background = "#B0A59C";
+        statsElement2Title.style.background = "#9CA6B0";
         statsElement2.innerText = gamesWon;
-        statsElement2.style.background = "#B0A59C"; 
+        statsElement2.style.background = "#9CA6B0"; 
         statsElement3Title.innerHTML = "Lost: ";
-        statsElement3Title.style.background = "#B0A59C";
+        statsElement3Title.style.background = "#9CA6B0";
         statsElement3.innerText = gamesLost;
-        statsElement3.style.background = "#B0A59C";
+        statsElement3.style.background = "#9CA6B0";
         // Style container 4
 
         // make reference to the html containers where the info will be displayed
-        container2.innerHTML = " ";
-        container2.appendChild(nameHeader);
-        container2.appendChild(nameElement);
-        container2.appendChild(realmHeader);
-        container2.appendChild(realmElement);
-        container2.appendChild(ratingHeader);
-        container2.appendChild(ratingElement);
-        container2.appendChild(statsElement1Title);
-        container2.appendChild(statsElement1);
-        container2.appendChild(statsElement2Title);
-        container2.appendChild(statsElement2);
-        container2.appendChild(statsElement3Title);
-        container2.appendChild(statsElement3);
+        container3.innerHTML = " ";
+        container3.appendChild(ratingHeader);
+        container3.appendChild(ratingElement);
+        container3.appendChild(statsElement1Title);
+        container3.appendChild(statsElement1);
+        container3.appendChild(statsElement2Title);
+        container3.appendChild(statsElement2);
+        container3.appendChild(statsElement3Title);
+        container3.appendChild(statsElement3);
 
       } catch {
         err => console.error("oops!", err.message);
@@ -152,9 +163,9 @@ async function getData(event) {
           let s2 = spec2.toLowerCase();
           let s3 = spec3.toLowerCase();
 
-      let url3 = `https://us.api.blizzard.com/profile/wow/character/${requestRealmName}/${requestCharName}/pvp-bracket/shuffle-${cClass}-${s1}?namespace=profile-us&locale=en_US&access_token=USkJV9J487uokW93PqhwB3Ka9CQCikPvtD`;
-      let response3 = await fetch(url3);
-      let shuffleData1 = await response3.json();
+      let url4 = `https://us.api.blizzard.com/profile/wow/character/${requestRealmName}/${requestCharName}/pvp-bracket/shuffle-${cClass}-${s1}?namespace=profile-us&locale=en_US&access_token=USXKXQSiqHxN6R39kxn3plUh5QWh3VFkio`;
+      let response4 = await fetch(url4);
+      let shuffleData1 = await response4.json();
       try {
         // Create HTML elements
         const shuffleTitle1 = document.createElement("h4");
@@ -179,19 +190,20 @@ async function getData(event) {
         shuffleTitle1.innerHTML = `${spec1} rating: `;
 
         // Make reference to the HTML containers where the info will be displayed
-        container3.appendChild(shuffleTitle1);
-        container3.appendChild(shuffleRating1);
-        container3.appendChild(shufflePlayed1);
-        container3.appendChild(shuffleWon1);
-        container3.appendChild(shuffleLost1);
+        container4.innerHTML = " ";
+        container4.appendChild(shuffleTitle1);
+        container4.appendChild(shuffleRating1);
+        container4.appendChild(shufflePlayed1);
+        container4.appendChild(shuffleWon1);
+        container4.appendChild(shuffleLost1);
       
       } catch {
         err => console.error("oops!", err.message);
       }
       
-      let url4 = `https://us.api.blizzard.com/profile/wow/character/${requestRealmName}/${requestCharName}/pvp-bracket/shuffle-${cClass}-${s2}?namespace=profile-us&locale=en_US&access_token=USkJV9J487uokW93PqhwB3Ka9CQCikPvtD`;
-      let response4 = await fetch(url4);
-      let shuffleData2 = await response4.json();
+      let url5 = `https://us.api.blizzard.com/profile/wow/character/${requestRealmName}/${requestCharName}/pvp-bracket/shuffle-${cClass}-${s2}?namespace=profile-us&locale=en_US&access_token=USXKXQSiqHxN6R39kxn3plUh5QWh3VFkio`;
+      let response5 = await fetch(url5);
+      let shuffleData2 = await response5.json();
       try {
         // Create HTML elements
         const shuffleTitle2 = document.createElement("h4");
@@ -216,19 +228,19 @@ async function getData(event) {
         shuffleTitle2.innerHTML = `${spec2} rating: `;
 
         // Make reference to the HTML containers where the info will be displayed
-        container3.appendChild(shuffleTitle2);
-        container3.appendChild(shuffleRating2);
-        container3.appendChild(shufflePlayed2);
-        container3.appendChild(shuffleWon2);
-        container3.appendChild(shuffleLost2);
+        container4.appendChild(shuffleTitle2);
+        container4.appendChild(shuffleRating2);
+        container4.appendChild(shufflePlayed2);
+        container4.appendChild(shuffleWon2);
+        container4.appendChild(shuffleLost2);
 
       } catch {
         err => console.error("oops!", err.message);
       }
       
-      let url5 = `https://us.api.blizzard.com/profile/wow/character/${requestRealmName}/${requestCharName}/pvp-bracket/shuffle-${cClass}-${s3}?namespace=profile-us&locale=en_US&access_token=USkJV9J487uokW93PqhwB3Ka9CQCikPvtD`;
-      let response5 = await fetch(url5);
-      let shuffleData3 = await response5.json();
+      let url6 = `https://us.api.blizzard.com/profile/wow/character/${requestRealmName}/${requestCharName}/pvp-bracket/shuffle-${cClass}-${s3}?namespace=profile-us&locale=en_US&access_token=USXKXQSiqHxN6R39kxn3plUh5QWh3VFkio`;
+      let response6 = await fetch(url6);
+      let shuffleData3 = await response6.json();
       try {
         // Create HTML elements
         const shuffleTitle3 = document.createElement("h4");
@@ -253,39 +265,25 @@ async function getData(event) {
         shuffleTitle3.innerHTML = `${spec3} rating: `;
 
         // Make reference to the HTML containers where the info will be displayed
-        container3.appendChild(shuffleTitle3);
-        container3.appendChild(shuffleRating3);
-        container3.appendChild(shufflePlayed3);
-        container3.appendChild(shuffleWon3);
-        container3.appendChild(shuffleLost3);
+        container4.appendChild(shuffleTitle3);
+        container4.appendChild(shuffleRating3);
+        container4.appendChild(shufflePlayed3);
+        container4.appendChild(shuffleWon3);
+        container4.appendChild(shuffleLost3);
 
       } catch {
         err => console.error("oops!", err.message);
       }
+    
+  /* This code is making an API request to the Blizzard API to fetch data about a World of Warcaft character's
+  achievements */
+      let url7 = `https://us.api.blizzard.com/profile/wow/character/${requestRealmName}/${requestCharName}/achievements?namespace=profile-us&locale=en_US&access_token=USXKXQSiqHxN6R39kxn3plUh5QWh3VFkio`;   
+      let response7 = await fetch(url7);
+      let achievementsData = await response7.json();
 
-/* This code is making an API request to the Blizzard API to fetch data about a World of Warcraft
-  character's avatar. */
-      
-      let url6 = `https://us.api.blizzard.com/profile/wow/character/${requestRealmName}/${requestCharName}/character-media?namespace=profile-us&locale=en_US&access_token=USkJV9J487uokW93PqhwB3Ka9CQCikPvtD`;
-      let response6 = await fetch(url6);
-      let avatarData = await response6.json();
-      try {
-        // Container Section 1 - Character Name
-        const avatarHeader = document.createElement("h3");
-        const avatarElement = document.createElement("img");
-        let avatarURL = await avatarData.assets[2].value;
-        avatarElement.src = avatarURL;
-
-        // Style container 1
-        avatarElement.style.width = "100%"
-        avatarElement.style.background = "black";
-
-        // make reference to the html containers where the info will be displayed
-        container4.innerHTML = " ";
-        container4.appendChild(avatarHeader);
-        container4.appendChild(avatarElement);
-      } catch {
-        err => console.error("oops!", err.message);
-      }
-
+  /* This code is making an API request to the Blizzard API to fetch data about a World of Warcraft
+  character's personal rating history. */
+      let url8 = `https://us.api.blizzard.com/profile/wow/character/${requestRealmName}/${requestCharName}/achievements/statistics?namespace=profile-us&locale=en_US&access_token=USXKXQSiqHxN6R39kxn3plUh5QWh3VFkio`;
+      let response8 = await fetch(url8);
+      let pvpHistoryData = await response8.json();
 }
